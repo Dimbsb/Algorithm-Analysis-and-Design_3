da6527c7da650da8566d2160b8291abe9f5ed9fc
Dimitrios Bismpas <121557787+Dimbsb@users.noreply.github.com>
2025-05-20 01:41:11 +0300

    Add files via upload

---
+++ b2.c
@@ -0,0 +1,8 @@
+/* 2. Implement the Kruskal algorithm from Lecture 10 using adjacency lists again.
+Call the function for the 4 graphs you used in the previous question and show
+schematically the minimum paired tree. Provide your code file and explain your
+implementation in the pdf file. Also, show a screenshot of the execution in the pdf file.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
