882e64a8de0637bb22fb41821e84d638a69f1947
Dimitrios Bismpas <121557787+Dimbsb@users.noreply.github.com>
2025-05-27 01:18:11 +0300

    Delete b3.c

--- b3.c
+++
@@ -1,8 +0,0 @@
-/* 3. Look for information about the “Reverse-delete algorithm” which is also used for
-finding a minimum mating tree. Implement the algorithm and compare computationally with 
-the previous two algorithms by creating a computational study. Provide your code file and
-explain your implementation in the pdf file. Finally, show screenshot of the execution in the pdf file.*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
